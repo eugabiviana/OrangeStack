3304485950f0b98138fc3070533dfd999dd4269e
eugabiviana <gabi.viana88@gmail.com>
2021-06-17 15:17:08 -0300

    Consuming endpoints on gRPC

--- fretes-rest/src/main/kotlin/br/com/zup/edu/GrpcClientFactory.kt
+++ fretes-rest/src/main/kotlin/br/com/zup/edu/GrpcClientFactory.kt
@@ -1,4 +1,26 @@
 package br.com.zup.edu
 
+import io.grpc.ManagedChannel
+import io.micronaut.context.annotation.Factory
+import io.micronaut.grpc.annotation.GrpcChannel
+import javax.inject.Singleton
+
+@Factory
 class GrpcClientFactory {
+
+    @Singleton
+    fun fretesClientStub(@GrpcChannel("fretes") channel: ManagedChannel): FretesServiceGrpc.FretesServiceBlockingStub? {
+
+//        val channel: ManagedChannel = ManagedChannelBuilder
+//                        .forAddress("localhost", 50051)
+//                        .usePlaintext()
+//                        .maxRetryAttempts(10)
+//                        .build()
+
+        return FretesServiceGrpc
+                .newBlockingStub(channel)
+    }
 }
+/**
+ * Essa linha de código: (@GrpcChannel("localhost:50051") exclui a necessidade do código comentado, pois faz a mesma coisa.
+ */
